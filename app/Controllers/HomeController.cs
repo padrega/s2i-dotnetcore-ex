846b5abee651cdc7f101d4aec540760efb2a07cc
silverdust <paulo.adrega@gmail.com>
2018-07-19 15:34:45 +0100

    Update HomeController.cs

--- app/Controllers/HomeController.cs
+++ app/Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace app.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "A página da minha descrição da aplicação.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "O meu contacto.";
 
             return View();
         }
